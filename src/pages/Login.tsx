61e7a40bbb2fd292f7f2eeef760483cd768030b8
5ock <ccrubio5@gmail.com>
2025-05-07 16:41:49 +0800

    Setting up react-router-dom

---
+++ src/pages/Login.tsx
@@ -0,0 +1,5 @@
+const Login = () => {
+    return (<div className='text-red-500'>login page</div>)
+}
+
+export default Login
---
+++ src/routes.tsx
@@ -0,0 +1,7 @@
+import type { RouteObject } from 'react-router-dom'
+import Login from './pages/Login'
+
+export const routes: RouteObject[] = [
+    { path: '/', element: <Login />},
+
+]
