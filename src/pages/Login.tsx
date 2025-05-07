2ed2cfc1f8156c92322c76e89c822100e35e58d5
5ock <ccrubio5@gmail.com>
2025-05-07 23:11:14 +0800

    Add i18n feature

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,12 @@
+import { useTranslation } from 'react-i18next'
+
 const Login = () => {
-    return (<div className='text-red-500'>login page</div>)
+    const { t, i18n } = useTranslation();
+
+    return (<>
+        <div className='text-red-500'>login page</div>
+        <div>{ t('login') }</div>
+    </>)
 }
 
 export default Login
